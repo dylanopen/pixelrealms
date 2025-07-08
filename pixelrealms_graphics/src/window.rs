24e92d18818f224c26fe61d107566376eb0d95b7
dylan <opendylan@proton.me>
2025-07-08 12:35:12 +0100

    add Window::new

--- pixelrealms_graphics/src/window.rs
+++ pixelrealms_graphics/src/window.rs
@@ -1,4 +1,15 @@
 pub struct Window {
-    minifb: minifb::Window,
+    minifb_window: minifb::Window,
+}
+
+impl Window {
+    pub fn new(width: usize, height: usize, title: &str) -> Result<Window, minifb::Error> {
+        let minifb_window = minifb::Window::new(title, width, height,
+            minifb::WindowOptions::default())?;
+
+        Ok(Window {
+            minifb_window
+        })
+    }
 }
 
