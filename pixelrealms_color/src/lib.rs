f3d7e06c3ba09f0758d810ce5dabca18ff2a2c54
dylan <opendylan@proton.me>
2025-07-08 09:28:58 +0100

    add pixelrealms_color crate

---
+++ pixelrealms_color/src/lib.rs
@@ -0,0 +1,19 @@
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub const BLACK: Color = Color::rgba(0, 0, 0, 255);
+    
+    pub const fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
+        Color { r, g, b, a }
+    }
+
+    pub const fn rgb(r: u8, g: u8, b: u8) -> Color {
+        Color::rgba(r, g, b, 255)
+    }
+}
+
